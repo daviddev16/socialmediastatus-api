db0eb5984a5e2da5f675877fd1b09314113b9cff
David <davidduartepinheiro@gmail.com>
2023-04-16 13:45:02 -0300

    Tirando "/devops/**" do endpoint já que vai ser reservado pelo endpoint do nginx.

--- src/main/java/live/socialmedia/controller/StatusController.java
+++ src/main/java/live/socialmedia/controller/StatusController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/devops/api/status")
+@RequestMapping("/api/status")
 public class StatusController {
 
     private LiveStatusService statusService;
